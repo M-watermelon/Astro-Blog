96754e1587e168b267b364c276c18f8b77232218
mwatermelon <m.watermelon20@gmail.com>
2025-01-03 10:31:44 -0600

    fix?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://m-watermelon.github.io/Astro-Blog/',
-  base: 'home',
+  base: 'https://m-watermelon.github.io/Astro-Blog/',
 })
